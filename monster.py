309739c28a8b55483a6b79921ce33810b5c6472f
Ryan Tolentino <Tolentino2016@gmail.com>
2016-07-29 11:37:13 -0400

    Edited Monster.py

---
+++ monster.py
@@ -0,0 +1,15 @@
+#Intro to inheritance
+
+import random
+
+COLORS = ['yellow', 'red', 'blue', 'green']
+
+class Monster:
+    min_hit_points = 1
+    max_hit_points = 1
+    min_experience = 1
+    max_experience = 1
+    weapon = 'sword'
+    sound = 'roar'
+
+    
