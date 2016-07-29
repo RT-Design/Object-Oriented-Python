8b6c9178c691382f40ef125da7ed1aea13842bf7
Ryan Tolentino <Tolentino2016@gmail.com>
2016-07-29 11:58:12 -0400

    edited monster class to show inheritance.

--- monster.py
+++ monster.py
@@ -12,4 +12,17 @@ class Monster:
     weapon = 'sword'
     sound = 'roar'
 
+    def __init__(self, **kwargs):
+        self.hit_points = random.randint(self.min_hit_points,self.max_hit_points)
+        self.experience = random.randint(self.min_experience, self.max_experience)
+        self.color = random.choice(COLORS)
+
+        for key, value in kwargs.items():
+            setattr(self,key,value)
+
+    def battlecry(self):
+
+        return self.sound.upper()
+        
+
     
